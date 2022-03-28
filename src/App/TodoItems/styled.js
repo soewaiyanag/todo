a9d5fc91f09b73efddf0d00bdf9f231402c62af3
Soe Wai Yan Aung <soewaiyanag@gmail.com>
2022-03-28 21:27:29 +0630

    add box shadow to todoitems

--- src/App/TodoItems/styled.js
+++ src/App/TodoItems/styled.js
@@ -3,6 +3,8 @@ import Box from 'shared/Box';
 
 const StyledTodoItems = styled(Box)`
   border-radius: ${({ theme }) => theme.sizes.borderRadius};
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px,
+    rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
   margin-top: 1rem;
   overflow: hidden;
   padding: 0;
