8cdf700e16b36342af453f80fc036cbc78df98e3
Soe Wai Yan Aung <soewaiyanag@gmail.com>
2022-04-01 10:55:40 +0630

    set theme by prefers-color-scheme

--- src/context/DarkContext.js
+++ src/context/DarkContext.js
@@ -1,9 +1,18 @@
-import { createContext, useState } from "react";
+import { createContext, useState } from 'react';
 
-const DarkContext = createContext("");
+const DarkContext = createContext('');
+
+// check if machine is using light mode or dark mode
+function preferDarkMode() {
+  return (
+    window.matchMedia &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches
+  );
+}
 
 export function DarkProvider({ children }) {
-  const [isDark, setIsDark] = useState(true);
+  const [isDark, setIsDark] = useState(preferDarkMode());
+
   return (
     <DarkContext.Provider value={{ isDark, setIsDark }}>
       {children}
